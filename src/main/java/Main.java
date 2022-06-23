6c76de82556a5fe03820511d51396e6ae21795e4
Troy <troy@Zipcoders-MacBook-Pro-28.local>
2022-06-23 17:49:07 -0400

    Worked on

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,9 +1,19 @@
 /**
  * Created by iyasuwatts on 10/17/17.
  */
+import java.util.Scanner;
 public class Main {
 
     public static void main(String[] args ){
+        Scanner name = new Scanner(System.in);
+        System.out.println("What is your name?");
 
+        String userName = name.nextLine();
+
+        if(userName.equalsIgnoreCase("Alice")){
+            System.out.println("Hi Alice!");
+        } else if(userName.equalsIgnoreCase("Bob")){
+            System.out.println("Hi Bob");
+        }
     }
 }
